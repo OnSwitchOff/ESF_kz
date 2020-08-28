5e12e50f7390cf7e5d4aa990963941c914701d5b
Viktor Kassov <viktor.kassov@microinvest.net>
2020-08-28 17:14:34 +0300

    Add project files.

---
+++ ESF_kz/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ESF_kz
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			string ialogResult = (((detailsESFfieldAttribute)Attribute.GetCustomAttribute((typeof(ESF)).GetField("registrationNumber"), typeof(detailsESFfieldAttribute))).details);
+			Application.Run(new Form1());
+			
+		}
+	}
+}
