93d6b871e9353f5cad412eb1d8acd1a0c7b9e24f
Viktor Kassov <kassov@mail.ua>
2020-10-02 11:42:40 +0300

    ResponseManager + invoiceV2 parsing

---
+++ ESF_kz/Facades/LogManagerFacade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESF_kz
+{
+	static class LogManagerFacade
+	{
+		internal static void ParsingXmlExeption(string v, string innerText, Exception e)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
